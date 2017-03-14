56fe6de2f9b2d993e7da4202b88f000e40d33939
xiaoming.wang <18600882246@163.com>
2017-03-14 18:02:48 +0800

    add fastjson

--- src/main/java/org/lightfw/utilx/ext/FasterXMLJsonUtil.java
+++ src/main/java/org/lightfw/utilx/ext/FasterXMLJsonUtil.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class JsonUtil {
+public class FasterXMLJsonUtil {
 
     private static ObjectMapper mapper = null;
 
