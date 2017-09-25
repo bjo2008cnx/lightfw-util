2a5424d913114d786cfce94a96c1a94543ff142d
xiaoming.wang <18600882246@163.com>
2017-09-25 17:09:58 +0800

    remove context name

--- light-util-extend/src/main/java/org/lightfw/utilx/spring/SpringBeanUtil.java
+++ light-util-extend/src/main/java/org/lightfw/utilx/spring/SpringBeanUtil.java
@@ -2,12 +2,11 @@ package org.lightfw.utilx.spring;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
-import org.springframework.beans.factory.access.BeanFactoryLocator;
-import org.springframework.beans.factory.access.BeanFactoryReference;
-import org.springframework.beans.factory.access.SingletonBeanFactoryLocator;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -20,8 +19,10 @@ import java.util.Map;
 @Log4j2
 public class SpringBeanUtil {
 
-    private static final String DEFAULT_FILE_NAME = "services.all.xml"; //TODO 参数传入
-    private static final String DEFAULT_CONTEXT = "default-context";
+    /**
+     * 如果默认的文件名路径不是classpath:service.xml，需要调用initContextFile进行配置
+     */
+    private static String DEFAULT_FILE_NAME = "service.xml";
     private static final String DEFAULT_DATA_SOURCE = "dataSource";
 
     /**
@@ -43,6 +44,10 @@ public class SpringBeanUtil {
      * 初始化时的锁，用于将调用getBean的消费者排队等待
      */
     public static Object lockInitFactory = new Object();
+
+    public static void initContextFile(String xmlFileName) {
+        DEFAULT_FILE_NAME = xmlFileName;
+    }
 
     /**
      * 返回bean数组
@@ -91,19 +96,9 @@ public class SpringBeanUtil {
         initBeanStarted = initBeanStarted_;
     }
 
-    /**
-     * 自执行初始化BeanFactory
-     */
-    private static void defaultInitBeanFactory() {
-
-        log.info("Init bean factory start................");
-        BeanFactoryLocator instance = null;
-        instance = SingletonBeanFactoryLocator.getInstance(DEFAULT_FILE_NAME);
-
-        BeanFactoryReference bfr = instance.useBeanFactory(DEFAULT_CONTEXT); // TODO
-        // 传入参数
-        beanFactory = bfr.getFactory();
-        log.info("Init bean factory end.");
+    private static void initBeanFactory(String... fileNames) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(fileNames);
+        beanFactory = context;
     }
 
     /**
@@ -111,12 +106,12 @@ public class SpringBeanUtil {
      *
      * @return
      */
-    public static BeanFactory getBeanFactory() {
+    public static BeanFactory getBeanFactory(String... contextFiles) {
         if (!isInitBean) {
             synchronized (lockInitFactory) { // lock and init
                 if (!isInitBean) {
                     setInitBeanStarted(true);
-                    defaultInitBeanFactory();
+                    initBeanFactory(contextFiles);
                     isInitBean = true;
                 }
             }
@@ -137,14 +132,16 @@ public class SpringBeanUtil {
         isInitBean = true;
     }
 
-    private static BeanFactory getBeanFactorySafe() {
+    private static BeanFactory getBeanFactorySafe(String... contextFiles) {
+        if (contextFiles == null || contextFiles.length == 0) {
+            contextFiles = new String[]{DEFAULT_FILE_NAME};
+        }
         if (!isInitBean && initBeanStarted) { // 如果未启动完毕，但已经开始启动，则不走getBeanFactory()防止触发创建第2个BeanFactory
             if (beanFactory != null) {
                 return beanFactory;
             }
-            synchronized (lockInitFactory) { // wait for process that initing
-                // beanFactory
-                // nothing
+            synchronized (lockInitFactory) {
+                //.do nothing but wait for process that init
             }
             if (!isInitBean) {
                 StringBuilder warnInfo = new StringBuilder("BeanHelper.getBeanFactorySafe(\"");
@@ -153,7 +150,7 @@ public class SpringBeanUtil {
                 return null;
             }
         }
-        return getBeanFactory();
+        return getBeanFactory(contextFiles);
     }
 
 
@@ -173,25 +170,6 @@ public class SpringBeanUtil {
         return null;
     }
 
-    /**
-     * Return the bean instance that uniquely matches the given object type, if
-     * any.
-     *
-     * @param requiredType type the bean must match; can be an interface or superclass.
-     *                     {@code null} is disallowed.
-     *                     <p/>
-     *                     This method goes into {@link ListableBeanFactory} by-type
-     *                     lookup territory but may also be translated into a
-     *                     conventional by-name lookup based on the name of the given
-     *                     type. For more extensive retrieval operations across sets of
-     *                     beans, use {@link ListableBeanFactory} and/or
-     *                     {@link BeanFactoryUtils}.
-     * @return an instance of the single bean matching the required type
-     * @throws NoSuchBeanDefinitionException   if no bean of the given type was found
-     * @throws NoUniqueBeanDefinitionException if more than one bean of the given type was found
-     * @see ListableBeanFactory
-     * @since 3.0
-     */
     public static <T> T getBean(Class<T> requiredType) throws BeansException {
         BeanFactory bf = getBeanFactorySafe();
         if (bf != null) {
@@ -200,25 +178,6 @@ public class SpringBeanUtil {
         return null;
     }
 
-    /**
-     * Return an instance, which may be shared or independent, of the specified
-     * bean.
-     * <p/>
-     * Allows for specifying explicit constructor arguments / factory method
-     * arguments, overriding the specified default arguments (if any) in the
-     * bean definition.
-     *
-     * @param name the name of the bean to retrieve
-     * @param args arguments to use if creating a prototype using explicit
-     *             arguments to a static factory method. It is invalid to use a
-     *             non-null args value in any other case.
-     * @return an instance of the bean
-     * @throws NoSuchBeanDefinitionException if there is no such bean definition
-     * @throws BeanDefinitionStoreException  if arguments have been given but the affected bean isn't a
-     *                                       prototype
-     * @throws BeansException                if the bean could not be created
-     * @since 2.5
-     */
     public static Object getBean(String name, Object... args) throws BeansException {
         BeanFactory bf = getBeanFactorySafe();
         if (bf != null) {
