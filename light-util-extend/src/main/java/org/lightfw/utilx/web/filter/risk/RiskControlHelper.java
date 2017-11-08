da7ffffebf7abad553b3f5ff28e493279ba40dbe
xiaoming.wang <18600882246@163.com>
2017-11-08 17:14:35 +0800

    refactor

---
+++ light-util-extend/src/main/java/org/lightfw/utilx/web/filter/risk/RiskControlHelper.java
@@ -0,0 +1,19 @@
+package org.lightfw.utilx.web.filter.risk;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * RiskControlHelper
+ *
+ * @author Michael.Wang
+ * @date 2017/11/8
+ */
+@Slf4j
+public class RiskControlHelper {
+    public static void validate(boolean toAssert, String message) {
+        if (!toAssert) {
+            log.info(message);
+            throw new RiskControlValidateException(message);
+        }
+    }
+}
--- light-util-extend/src/main/java/org/lightfw/utilx/web/filter/risk/RiskControlValidater.java
+++ light-util-extend/src/main/java/org/lightfw/utilx/web/filter/risk/RiskControlValidater.java
@@ -5,7 +5,6 @@ import org.lightfw.utilx.web.UrlUtil;
 import org.lightfw.utilx.web.request.RequestUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedSet;
@@ -27,29 +26,16 @@ public class RiskControlValidater {
      * 校验是否需要风控
      *
      * @param request
-     * @param resp
      */
-    public static void validate(HttpServletRequest request, HttpServletResponse resp) {
-        validate(RequestUtil.isGetRequest(request), "GET Request is IGNORED.");
-        validate(RequestUtil.isUploadRequest(request), "Uploading Request is IGNORED.");
-        validate(isExcludedPage(request), "Excluded page is IGNORED.");
-        validate(isRiskOff(), "Risk Control switch is off..");
+    public static void validate(HttpServletRequest request) {
+        RiskControlHelper.validate(RequestUtil.isGetRequest(request), "GET Request is IGNORED.");
+        RiskControlHelper.validate(RequestUtil.isUploadRequest(request), "Uploading Request is IGNORED.");
+        RiskControlHelper.validate(isExcludedPage(request), "Excluded page is IGNORED.");
     }
 
     public static boolean isExcludedPage(HttpServletRequest request) {
         String path = request.getServletPath();
         SortedSet excludedPages = (SortedSet) RISK_CONTROL_CONFIG.get("excludedPages");
         return UrlUtil.urlMatch(excludedPages, path);
-    }
-
-    public static boolean isRiskOff() {
-        return false;
-    }
-
-    private static void validate(boolean toAssert, String message) {
-        if (!toAssert) {
-            log.info(message);
-            throw new RiskControlValidateException(message);
-        }
     }
 }
