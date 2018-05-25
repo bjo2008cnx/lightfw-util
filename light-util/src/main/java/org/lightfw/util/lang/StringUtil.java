f9a5a79e5c2a45c47b4885485cc2bbdcb4c3d01d
xiaoming.wang <18600882246@163.com>
2018-05-25 16:50:09 +0800

    移动方法位置

--- light-util/src/main/java/org/lightfw/util/lang/StringUtil.java
+++ light-util/src/main/java/org/lightfw/util/lang/StringUtil.java
@@ -29,6 +29,10 @@ public class StringUtil {
     public static final char UNDERLINE = '_';
     private static final String DEFAULT_ENCODING = GlobalConstant.CharSets.DEFAULT_ENCODING;
 
+    // 用于驼峰转下划线
+    private static Pattern linePattern = Pattern.compile("_(\\w)");
+    // 用于驼峰转下划线
+    private static Pattern humpPattern = Pattern.compile("[A-Z]");
 
     /**
      * <p>
@@ -1689,5 +1693,39 @@ public class StringUtil {
             }
         }
         return builder.toString();
+    }
+
+    /**
+     * 下划线转驼峰
+     */
+    public static String underLineToCamel(String str) {
+        str = str.toLowerCase();
+        Matcher matcher = linePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+    /**
+     * 驼峰转下划线(简单写法，效率低于{@link #camelToUnderLine(String)})
+     */
+    public static String camelToUnderLineSimple(String str) {
+        return str.replaceAll("[A-Z]", "_$0").toLowerCase();
+    }
+
+    /**
+     * 驼峰转下划线,效率比上面高
+     */
+    public static String camelToUnderLine(String str) {
+        Matcher matcher = humpPattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
     }
 }
--- light-util/src/main/java/org/lightfw/util/text/StringExtUtil.java
+++ light-util/src/main/java/org/lightfw/util/text/StringExtUtil.java
@@ -3,7 +3,6 @@ package org.lightfw.util.text;
 import org.lightfw.util.lang.StringUtil;
 
 import java.text.DecimalFormat;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -11,15 +10,7 @@ import java.util.regex.Pattern;
  */
 public class StringExtUtil {
 
-    /**
-     * 用于驼峰转下划线
-     */
-    private static Pattern linePattern = Pattern.compile("_(\\w)");
 
-    /**
-     * 用于驼峰转下划线
-     */
-    private static Pattern humpPattern = Pattern.compile("[A-Z]");
 
     /**
      * 功能：将半角的符号转换成全角符号.(即英文字符转中文字符)
@@ -224,38 +215,5 @@ public class StringExtUtil {
         return df.format(f);
     }
 
-    /**
-     * 下划线转驼峰
-     */
-    public static String underLineToCamel(String str) {
-        str = str.toLowerCase();
-        Matcher matcher = linePattern.matcher(str);
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
-        }
-        matcher.appendTail(sb);
-        return sb.toString();
-    }
-
-    /**
-     * 驼峰转下划线(简单写法，效率低于{@link #camelToUnderLine(String)})
-     */
-    public static String camelToUnderLineSimple(String str) {
-        return str.replaceAll("[A-Z]", "_$0").toLowerCase();
-    }
-
-    /**
-     * 驼峰转下划线,效率比上面高
-     */
-    public static String camelToUnderLine(String str) {
-        Matcher matcher = humpPattern.matcher(str);
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
-        }
-        matcher.appendTail(sb);
-        return sb.toString();
-    }
 
 }
